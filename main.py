0cc2d73ac79d5fad304fde77408c970793a7d676
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-07-05 00:27:40 +0530

    get input from user

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-user_choice = "rock"
-computer_choice = "paper"
-both_choice = "scisor"
+import random
 
-def test_function():
-    return "Hi Sahan"
-
-print(test_function())
+def get_choice():
+    player_choice = input("Enter a choice (rock, paper, scissors): ")
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
+    choices = {"player": player_choice, "computer": computer_choice}
+    return choices
